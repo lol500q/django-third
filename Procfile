e2e27e163ad3b4f9a7083ff3f2df1a3e112de382
lol500q <mohmmadayesh22@gmail.com>
2022-02-07 15:39:11 +0200

    add Procfile and set allowed host also install gunicorn

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn django13.wsgi --log-file-
--- django13/settings.py
+++ django13/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dzi#0#ldq$6e6@5mlowzp5$eb&69#=zxc1n^7gz@+lof19o)=!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
